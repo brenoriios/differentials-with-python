764c7bbb6e348ca22c956ae3280e4298dc74e189
Breno Rios <breno@brenorios.com.br>
2024-10-30 18:11:04 -0300

    Some changes

--- Views/GUI.py
+++ Views/GUI.py
@@ -1,11 +1,14 @@
 import tkinter as tk
+import sympy as sp
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from Models.Differential import *
 
 class GUI:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Calculadora de Derivadas Parciais")
-        self.root.minsize(350, 350)
+        self.root.minsize(450, 350)
 
         self.init_gui()
         self.pack_gui()
@@ -19,7 +22,8 @@ class GUI:
 
         self.tip_label = tk.Label(
             master = self.main_frame,
-            text = "Digite a expressão de uma função:",
+            text = "Digite a expressão de uma função de duas ou mais variáveis:",
+            font=("TkDefaultFont", 13)
         )
 
         self.controls_frame = tk.Frame(
@@ -27,12 +31,13 @@ class GUI:
         )
 
         self.expr_input = tk.Entry(
-            master = self.controls_frame
+            master = self.controls_frame,
+            font=("TkDefaultFont", 13)
         )
 
         self.calculate_button = tk.Button(
             master = self.controls_frame,
-            text = "Calcular"
+            text = "Calcular",
         )
 
         self.result_frame = tk.Frame(
@@ -41,49 +46,53 @@ class GUI:
             pady=10
         )
 
+        self.current_function = tk.Frame(
+            master = self.result_frame
+        )
+
         self.current_function_label = tk.Label(
-            master = self.result_frame,
+            master = self.current_function,
+            text = "Função Considerada:",
             justify="left",
-            pady=10
+            font=("TkDefaultFont", 13)
+        )
+
+        self.first_order_diff_result = tk.Frame(
+            master = self.result_frame
         )
 
         self.first_order_diff_label = tk.Label(
-            master = self.result_frame,
+            master = self.first_order_diff_result,
             text = "Derivadas parciais de primeira ordem:",
-            justify="left"
+            justify="left",
+            font=("TkDefaultFont", 13)
         )
 
-        self.first_order_diff_result = tk.Label(
-            master = self.result_frame,
-            justify="left",
-            pady=10
+        self.second_order_diff_result = tk.Frame(
+            master = self.result_frame
         )
 
         self.second_order_diff_label = tk.Label(
             master = self.result_frame,
             text = "Derivadas parciais de segunda ordem:",
-            justify="left"
-        )
-
-        self.second_order_diff_result = tk.Label(
-            master = self.result_frame,
             justify="left",
-            pady=10
+            font=("TkDefaultFont", 13)
         )
 
-        self.tip_label_two = tk.Label(
+        self.tip_label = tk.Label(
             master = self.main_frame,
-            text = "Dica: Não esqueça de colocar o operador * entre uma constante e uma variável. Ex.: 2*x",
+            text = "",
+            font=("TkDefaultFont", 13)
         )
 
     
     def pack_gui(self):
-        self.tip_label.pack()
         self.expr_input.pack(side="left", fill="x", expand=True)
         self.calculate_button.pack(side="right")
 
         self.controls_frame.pack(fill="x")
 
+        self.current_function.pack(fill="x")
         self.current_function_label.pack(fill="x")
 
         self.first_order_diff_label.pack(fill="x")
@@ -92,28 +101,68 @@ class GUI:
         self.second_order_diff_label.pack(fill="x")
         self.second_order_diff_result.pack(fill="x")
 
-        self.tip_label_two.pack()
+        self.tip_label.pack()
         self.main_frame.pack(fill="both", expand=True)
 
-    def set_first_order_diffs(self, results):
+    def set_first_order_diffs(self, results, params = ""):
         formatted_result_list = []
 
         for result in results:
-            formatted_result_list.append(f"∂f/∂{result.variable} = {result.expr.replace('**', '^')}")
+            formatted_result_list.append(f"∂f({params})/∂{result.variable} = {result.expr.replace('**', '^')}")
 
         self.first_order_diff_result.config(text = "\n".join(formatted_result_list))
 
-    def set_second_order_diffs(self, results):
+    def set_second_order_diffs(self, results, params = ""):
         formatted_result_list = []
 
         for result in results:
-            formatted_result_list.append(f"∂²f/∂{result.variable}∂{result.second_variable} = {result.expr.replace('**', '^')}")
+            formatted_result_list.append(f"∂²f({params})/∂{result.variable}∂{result.second_variable} = {result.expr.replace('**', '^')}")
 
         self.second_order_diff_result.config(text = "\n".join(formatted_result_list))
 
-    def show_results(self, expression):
-        self.current_function_label.config(text = expression)
+    def show_results(self, funcExpr, firstOrderDiffs, secondOrderDiffs, variables):
+        self.print_function(self.current_function, funcExpr, variables)
+
+        for diff in firstOrderDiffs:
+            self.print_function(self.first_order_diff_result,diff.expr, variables, diff.variable)
+        
+        for diff in secondOrderDiffs:
+            self.print_function(self.second_order_diff_result, diff.expr, variables, diff.variable, diff.second_variable)
+
         self.result_frame.pack(fill="x", expand=True)
+
+    def print_function(self, element, expr, variables, first_var = "", second_var = ""):
+        vars = ""
+
+        if first_var != "":
+            vars = "_{" + first_var + "}"
+
+        if second_var != "":
+            vars = "_{" + first_var + second_var + "}"
+
+        function_expr = sp.sympify(expr)
+        function_latex = f"$f{vars}({variables}) = {sp.latex(function_expr)}$"
+        
+        fig, ax = plt.subplots(figsize=(5, 0.4))
+        ax.axis('off')
+        ax.text(0.5, 0.5, function_latex, fontsize=13, ha='center', va='center')
+        canvas = FigureCanvasTkAgg(fig, master = element)
+        canvas.draw()
+        canvas.get_tk_widget().pack(padx=0, pady=0)
+        plt.close(fig)
+
+    def clear_results(self):
+        for widget in self.current_function.winfo_children():
+            if not isinstance(widget, tk.Label):
+                widget.destroy()
+
+        for widget in self.first_order_diff_result.winfo_children():
+            if not isinstance(widget, tk.Label):
+                widget.destroy()
+
+        for widget in self.second_order_diff_result.winfo_children():
+            if not isinstance(widget, tk.Label):
+                widget.destroy()
 
     def run(self):
         self.root.mainloop()
